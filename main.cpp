aea7b5f669a18c6c48fdca0b07fd5c87680841e9
alexchen116688 <alexchen116688@gmail.com>
2025-04-19 17:21:43 +0800

    pokemon

--- main.cpp
+++ main.cpp
@@ -3,10 +3,14 @@
 #include <QApplication>
 #include <QLocale>
 #include <QTranslator>
+#include <QLabel>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QLabel *label = new QLabel("第一行文字\n第二行文字");
+    label->setWordWrap(true); // 允許 QLabel 在空間不足時自動換行
 
     QTranslator translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
