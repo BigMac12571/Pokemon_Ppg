f39667b8151e6f41c4a8f6b203e0f26297abf8b3
alexchen116688 <alexchen116688@gmail.com>
2025-04-23 13:24:18 +0800

    bag centered

--- bag.cpp
+++ bag.cpp
@@ -2,8 +2,9 @@
 
 Bag::Bag(QWidget *parent) : QLabel(parent)
 {
-    this->setFixedSize(148, 189);
+    this->setFixedSize(222, 284);
 
     QPixmap pixmap(":/new/prefix1/Dataset/Image/bag.png");
     this->setPixmap(pixmap.scaled(size(), Qt::KeepAspectRatio, Qt::SmoothTransformation));
+    this->move(152,84);
 }
